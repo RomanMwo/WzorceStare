f8b573ed98e64025cb531ad0f0e8a5f9ee50f8bf
student15 <student15@pcoip.ki.agh.edu.pl>
2018-02-25 13:43:36 +0100

    strategia przed singletonem

--- faktury/src/dokumenty/Faktura.java
+++ faktury/src/dokumenty/Faktura.java
@@ -4,12 +4,18 @@ import java.util.Iterator;
 import java.util.Date;
 
 import magazyn.Towar;
+import rabaty.IObliczCenePoRabacie;
+import rabaty.ObliczCenePoRabacieKwotowym;
+import rabaty.ObliczCenePoRabacieProcentowym;;
 
 
 public class Faktura {
 	Date dataSprzedazy;
 	String kontrahent;
 	ArrayList<Pozycja> pozycje;
+	IObliczCenePoRabacie rabat;
+	
+	
 	double suma;
 	public Faktura(Date dataSprzedazy,String kontrahent)
 	{
@@ -17,6 +23,7 @@ public class Faktura {
 		this.kontrahent=kontrahent;
 		pozycje=new ArrayList<Pozycja>();
 		suma=0;
+		rabat = new ObliczCenePoRabacieKwotowym();
 	}
 	public void dodajPozycje(Towar towar, double ilosc)
 	{
@@ -43,6 +50,7 @@ public class Faktura {
 			pozycja = iteratorPozycji.next();
 			suma+=pozycja.getWartosc();
 		}
+	    suma = rabat.obliczCenePoRabacie(suma);
 	}
 	public Iterator<Pozycja> getIteratorPozycji()
 	{
--- faktury/src/rabaty/ObliczCenePoRabacieProcentowym.java
+++ faktury/src/rabaty/ObliczCenePoRabacieProcentowym.java
@@ -4,8 +4,8 @@ public class ObliczCenePoRabacieProcentowym implements IObliczCenePoRabacie {
 
 	@Override
 	public double obliczCenePoRabacie(double cena) {
-		cena = Math.abs(70*cena/100);
-		return 0;
+		cena = cena - Math.abs(70*cena/100);
+		return cena;
 	}
 
 }
